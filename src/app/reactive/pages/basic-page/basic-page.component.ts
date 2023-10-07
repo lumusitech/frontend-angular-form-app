d96d70af521c384370a262f923a5ce597d140ff7
lumusitech <lumusitech@gmail.com>
2023-10-07 02:14:45 -0300

    feat: add basic form with validations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// from backend using some data of path ie.
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2000,
+  inStorage: 5,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  // better way
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // loaded from backend using some data of path ie.
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return `${field} field is required`;
+        case 'minlength':
+          return `At least ${errors['minlength'].requiredLength} characters`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
 }
